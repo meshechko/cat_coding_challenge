dc55b130c751652e4124b90d058b8054ae14117b
Olexiy Meshechko <olexiy_meshechko@trimble.com>
2025-11-15 12:48:34 +1300

    Add map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from "./map/map.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cat_coding_challenge';
+  title = 'CAT coding challenge';
 }
