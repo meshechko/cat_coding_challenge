95e10962840dac10988b07e2deb3d98f8bdded4d
Olexiy Meshechko <olexiy_meshechko@trimble.com>
2025-11-15 15:49:25 +1300

    Initialize canvas

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { MapService } from './map.service';
 
 @Component({
@@ -10,9 +10,19 @@ import { MapService } from './map.service';
 export class MapComponent {
   private readonly mapService = inject(MapService);
 
-  ngOnInit() {
-    this.mapService.loadMap$().subscribe(mapInfo => {
-      console.log('Map Info:', mapInfo);
-    });
+  private readonly canvasRef = viewChild.required<ElementRef>('map');
+  private mapContext!: CanvasRenderingContext2D;
+
+  public ngOnInit(): void {  
+    this.initializeCanvas();
+  }
+
+  private initializeCanvas(): void {
+    const canvas = this.canvasRef().nativeElement;
+    this.mapContext = canvas.getContext('2d');
+    canvas.width = 1000;
+    canvas.height = 800;
+    this.mapContext.fillStyle = '#e9e9e9ff';
+    this.mapContext.fillRect(0, 0, canvas.width, canvas.height);
   }
 }
