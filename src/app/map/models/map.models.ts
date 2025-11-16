0c106f19c87fec11dda668ea29c322b6027d52fb
Olexiy Meshechko <olexiy_meshechko@trimble.com>
2025-11-16 17:18:04 +1300

    Add truck state interface

--- src/app/map/models/map.models.ts
+++ src/app/map/models/map.models.ts
@@ -27,3 +27,20 @@ export enum ZoneType {
   Loading = 'loading',
   Dumping = 'dumping'
 }
+
+export enum TruckStatus {
+  Loading = 'loading',
+  Hauling = 'hauling',
+  Dumping = 'dumping',
+  Idle = 'idle'
+}
+
+export interface TruckState {
+  id: string;
+  speed: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  status: TruckStatus;
+}
